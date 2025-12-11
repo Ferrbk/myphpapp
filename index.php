7b1d3c6462f2b06d113cd2c474edabd415679b94
firas ben kraiem <ferrbenkraiem@gmail.com>
2025-12-11 01:34:23 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Project</title>
+</head>
+<body>
+    <h1><?php echo "Welcome to my PHP project!"; ?></h1>
+    <p>هذا مشروع PHP بسيط يعمل على Render.</p>
+</body>
+</html>
